43ceba88f1863ac56d6ddc67c8a72fa6a36f43fa
Valeri Hristov <valericfbg@gmail.com>
2021-03-07 00:51:35 +0200

    Removes redundant comments, fixes script

--- docker-start.sh
+++ docker-start.sh
@@ -2,15 +2,12 @@
 
 set -x
 
-# Create a new image version with latest code changes.
 docker build . --tag pleo-antaeus
 
-# Build the code.
 docker run \
   --publish 7000:7000 \
   --rm \
   --interactive \
   --tty \
-  # This volume is only there so incremental builds are way faster
   --volume pleo-antaeus-build-cache:/root/.gradle \
   pleo-antaeus
